8e547d331d3094e8f2a1b8c236d7c1c351b2e2a1
mjdemars <mdemars7@gatech.edu>
2021-10-23 23:22:27 -0400

    Update topnav

--- subtract.js
+++
@@ -1,9 +0,0 @@
-function subtract() {
-    var amount = parseInt(document.getElementById("amount"));
-    var minus = parseInt(document.getElementById("subtract"));
-    if (amount - minus> 0) {
-        console.log("fail");
-        document.getElementById("amount").value = amount - minus;
-        console.log("success");
-    }
-}
